f377be2acf71de7e5a5a400c12f08cd75225260f
ahmedali <ahmeedali332332@gmail.com>
2025-03-11 14:21:47 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/prepai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prepai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:prepai/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
