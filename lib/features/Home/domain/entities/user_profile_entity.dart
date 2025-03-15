eb52aba5d8e1621624d0fcba0aeeb22c8de5a8cf
moemen <memmoh76@gmail.com>
2025-03-15 18:04:20 +0200

    added user profile entity

---
+++ lib/features/Home/domain/entities/user_profile_entity.dart
@@ -0,0 +1,14 @@
+class UserProfileEntity {
+  final String userImage;
+  final String userImail;
+  final String userName;
+  final String userPassword;
+  final String userPhone;
+
+  UserProfileEntity(
+      {required this.userImage,
+      required this.userImail,
+      required this.userName,
+      required this.userPassword,
+      required this.userPhone});
+}
