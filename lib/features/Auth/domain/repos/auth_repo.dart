459476dd8344c985720476a5308bcc8f3dc9c007
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-03-17 15:32:30 +0200

    added login use case and method in repo

--- lib/features/Auth/domain/repos/auth_repo.dart
+++ lib/features/Auth/domain/repos/auth_repo.dart
@@ -3,4 +3,8 @@ import '../models/user_model.dart';
 
 abstract class AuthRepository {
   Future<Either<String, UserModel>> signUp(String name, String email, String password, String phone);
+  
+  Future<Either<String, UserModel>> login(String email, String password);
+
+  Future<void> logout();
 }
