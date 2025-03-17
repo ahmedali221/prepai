4134d933b9cf85e1c46279e15183bf60ac69098e
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-03-17 17:17:53 +0200

    added widgets with fixed typos

---
+++ lib/features/Auth/presentation/widgets/social_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SocialButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String image;
+  final Color color;
+
+  const SocialButton({
+    super.key,
+    required this.onTap,
+    required this.color,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 30, // Image size relative to button
+            height: 30,
+            child: Image.asset(
+              image,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
