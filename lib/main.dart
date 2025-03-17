f6d3ad389001d11df949ba9705e046ad7855629c
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-03-17 15:57:56 +0200

    added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prepai/app.dart';
+import 'package:prepai/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ProviderScope(
