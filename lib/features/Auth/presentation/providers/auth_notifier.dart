aaa0a6d84cc8068b607b723de034d0478b9a1af5
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-03-17 13:49:28 +0200

    auth notifier

---
+++ lib/features/Auth/presentation/providers/auth_notifier.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:prepai/features/Auth/domain/repos/auth_repo.dart';
+import 'package:prepai/features/Auth/presentation/providers/auth_state.dart';
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthRepository authRepository;
+
+  AuthNotifier(this.authRepository) : super(AuthState.initial());
+
+  Future<void> signUp(
+      String name, String email, String password, String phone) async {
+    state = AuthState.loading(); 
+
+    final result = await authRepository.signUp(name, email, password, phone);
+
+    result.fold(
+      (error) => state = AuthState.error(error), 
+      (user) => state = AuthState.authenticated(user), 
+    );
+  }
+}
