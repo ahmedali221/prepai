70fdc481dea4b309940a5db1517b9c8fa9937801
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-03-17 15:39:41 +0200

    added splash page

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:prepai/features/Meals/presentation/meal_page.dart';
 
 
 class AppRouter {
---
+++ lib/features/Splash & Onboarding/presentation/splash_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:prepai/Core/theme/App_Colors.dart';
+import 'package:prepai/core/routes/app_routes.dart';
+import 'package:prepai/features/Splash%20&%20Onboarding/presentation/widgets/splash_page_body.dart';
+import 'package:go_router/go_router.dart'; 
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      // ignore: use_build_context_synchronously
+      context.go(AppRouter.kLogin); 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.secondaryColor,
+      body: const SplashPageBody(),
+    );
+  }
+}
