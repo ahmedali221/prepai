99e7823323033ad9ff04e4ad6f2756c0546f5928
Mohamed-Abd-El-Ghany <mb0672922@gmail.com>
2025-03-20 16:17:52 +0200

    Refactor app structure and update asset paths

--- lib/Core/utils/assets.dart
+++ lib/Core/utils/assets.dart
@@ -1,11 +1,11 @@
 class AppAssets {
-  static const String MainImage = 'images/Logo.png';
-  static const String editIcon = "images/EditIcon.png";
-  static const String maskGroupIcon = "images/MaskGroupIcon.png";
-  static const String homeIcon = "images/HomeIcon.png";
-  static const String personIcon = "images/PersonIcon.png";
-  static const String favouriteIcon = "images/FavouriteIcon.png";
-  static const String logoutIcon = "assets/LogoutIcon.png";
+  static const String MainImage = 'assets/images/Logo.png';
+  static const String editIcon = "assets/images/EditIcon.png";
+  static const String maskGroupIcon = "assets/images/MaskGroupIcon.png";
+  static const String homeIcon = "assets/images/HomeIcon.png";
+  static const String personIcon = "assets/images/PersonIcon.png";
+  static const String favouriteIcon = "assets/images/FavouriteIcon.png";
+  static const String logoutIcon = "assets/images/LogoutIcon.png";
   static const String mainImage = 'assets/images/Logo.png';
   static const String backgroundImage = 'assets/images/background.png';
   static const String facebook = 'assets/images/Facebook.png';
