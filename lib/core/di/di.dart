bb01f707a904f4a2ae32718dcb40ecba70b6256f
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-03-22 16:11:32 +0200

    added dependency injection file in core and auth use case

---
+++ lib/core/di/di.dart
@@ -0,0 +1,36 @@
+import 'package:get_it/get_it.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:prepai/core/services/firebase_service.dart';
+import 'package:prepai/features/Auth/data/data%20source/auth_remote_data_source.dart';
+import 'package:prepai/features/Auth/data/repos/auth_repo_impl.dart';
+import 'package:prepai/features/Auth/domain/repos/auth_repo.dart';
+import 'package:prepai/features/Auth/domain/use_cases/auh_use_case.dart';
+
+final getIt = GetIt.instance;
+
+void setupLocator() {
+  // Core Services
+  getIt.registerLazySingleton<FirebaseService>(
+    () => FirebaseService(
+      firebaseAuth: FirebaseAuth.instance,
+      firestore: FirebaseFirestore.instance,
+    ),
+  );
+
+  // Data Sources
+  getIt.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(getIt<FirebaseService>().firebaseAuth,
+        getIt<FirebaseService>().firestore),
+  );
+
+  // Repository
+  getIt.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(getIt<AuthRemoteDataSource>()),
+  );
+
+  // Use Cases
+  getIt.registerLazySingleton<AuthUseCase>(
+    () => AuthUseCase(getIt<AuthRepository>()),
+  );
+}
