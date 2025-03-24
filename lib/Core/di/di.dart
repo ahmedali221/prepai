63b49ffd979b1de7c008e2ed33bc302ac2c680dd
MohammedCV99 <mohammedabdoucv@gmail.com>
2025-03-25 02:53:22 +0300

    Add files via upload

--- lib/Core/di/di.dart
+++ lib/Core/di/di.dart
@@ -1,10 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:prepai/core/services/firebase_service.dart';
+import 'package:prepai/Core/services/firebase_service.dart';
 import 'package:prepai/features/Auth/data/data%20source/auth_remote_data_source.dart';
 import 'package:prepai/features/Auth/data/repos/auth_repo_impl.dart';
 import 'package:prepai/features/Auth/domain/repos/auth_repo.dart';
+import 'package:prepai/features/Meals/data/data_source/meal_remote_dataSource.dart';
+import 'package:prepai/features/Meals/data/repository/meal_repoImp.dart';
+import 'package:prepai/features/Meals/domain/repos/mealRepo.dart';
+import 'package:prepai/features/Meals/domain/use_cases/mealsUseCases.dart';
 import 'package:prepai/features/auth/domain/use_cases/auh_use_case.dart';
 
 final getIt = GetIt.instance;
@@ -17,7 +21,9 @@ void setupLocator() {
       firestore: FirebaseFirestore.instance,
     ),
   );
-
+  getIt.registerLazySingleton<MealRemoteDataSource>(
+    () => MealRemoteDataSourceimp(getIt<FirebaseService>()),
+  );
   // Data Sources
   getIt.registerLazySingleton<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(getIt<FirebaseService>()),
@@ -27,9 +33,15 @@ void setupLocator() {
   getIt.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(getIt<AuthRemoteDataSource>()),
   );
+  getIt.registerLazySingleton<MealRepository>(
+    () => MealRepositoryImpl(getIt<MealRemoteDataSource>()),
+  );
 
   // Use Cases
   getIt.registerLazySingleton<AuthUseCase>(
     () => AuthUseCase(getIt<AuthRepository>()),
+  );
+  getIt.registerLazySingleton<GetMealsUseCase>(
+    () => GetMealsUseCase(getIt<MealRepository>()),
   );
 }
