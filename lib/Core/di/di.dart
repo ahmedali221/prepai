d34e0bab2bdb5941d6f7f3d4c136418adb0a11b5
moemen <memmoh76@gmail.com>
2025-03-24 17:09:43 +0200

    solved conflicts

--- lib/Core/di/di.dart
+++ lib/Core/di/di.dart
@@ -2,9 +2,9 @@ import 'package:get_it/get_it.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:prepai/core/services/firebase_service.dart';
-import 'package:prepai/features/auth/data/data%20source/auth_remote_data_source.dart';
-import 'package:prepai/features/auth/data/repos/auth_repo_impl.dart';
-import 'package:prepai/features/auth/domain/repos/auth_repo.dart';
+import 'package:prepai/features/Auth/data/data%20source/auth_remote_data_source.dart';
+import 'package:prepai/features/Auth/data/repos/auth_repo_impl.dart';
+import 'package:prepai/features/Auth/domain/repos/auth_repo.dart';
 import 'package:prepai/features/auth/domain/use_cases/auh_use_case.dart';
 
 final getIt = GetIt.instance;
