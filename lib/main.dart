1e209a0969ff8ad986be9fd68d88917a40318ef2
moemen <memmoh76@gmail.com>
2025-03-26 13:47:28 +0200

    finished profile feature after refacoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prepai/Core/di/di.dart';
-import 'package:prepai/Core/services/firebase_service.dart';
 import 'package:prepai/app.dart';
 import 'package:prepai/firebase_options.dart';
 
