f5897de2d51ae74004138280516b0d526ae31266
moemen <memmoh76@gmail.com>
2025-03-26 17:34:58 +0200

    updated firebase service file

--- lib/Core/di/di.dart
+++ lib/Core/di/di.dart
@@ -1,6 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:prepai/core/services/firebase_service.dart';
 import 'package:prepai/features/Auth/data/data%20source/auth_remote_data_source.dart';
 import 'package:prepai/features/Auth/data/repos/auth_repo_impl.dart';
@@ -10,13 +8,6 @@ import 'package:prepai/features/auth/domain/use_cases/auh_use_case.dart';
 final getIt = GetIt.instance;
 
 void setupLocator() {
-  // Core Services
-  getIt.registerLazySingleton<FirebaseService>(
-    () => FirebaseService(
-      firebaseAuth: FirebaseAuth.instance,
-      firestore: FirebaseFirestore.instance,
-    ),
-  );
 
   // Data Sources
   getIt.registerLazySingleton<AuthRemoteDataSource>(
