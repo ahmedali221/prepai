81b11ec7e5ad2ebc65da73a47aa4b9ec837bce8a
moemen <memmoh76@gmail.com>
2025-03-29 00:49:33 +0200

    added navigations to side bar solved errors

--- lib/features/Splash & Onboarding/presentation/splash/splash_page.dart
+++ lib/features/Splash & Onboarding/presentation/splash/splash_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'package:prepai/Core/helper/check_first_seen.dart';
 import 'package:prepai/Core/theme/App_Colors.dart';
-import 'package:prepai/core/routes/app_routes.dart';
+
 import 'package:prepai/features/Splash%20&%20Onboarding/presentation/splash/widgets/splash_page_body.dart';
-import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
